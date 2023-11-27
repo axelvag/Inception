3adbbb82a97d2f21bac7f6d5e31c6b3ff3ce4f0b
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-27 15:52:35 +0100

    enlever le exec

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,11 +3,13 @@
 # GRANT ALL PRIV pour donner tous les droits a l'utilisateur
 # FLUSH PRIVILEGES pour appliquer les changements
 
-service mysql start;
+# service mysql start;
+mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mysqld_safe
+# exec mysqld_safe
