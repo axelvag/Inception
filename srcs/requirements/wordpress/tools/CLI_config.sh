8f29f67d376e8eec4a40c42f75800d80c3aa160a
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-29 11:16:42 +0100

    first CLI_config.sh

--- srcs/requirements/wordpress/tools/CLI_config.sh
+++ srcs/requirements/wordpress/tools/CLI_config.sh
@@ -1,17 +1,17 @@
 # on attend que mariaDb soit up
 sleep 10
 
-# # on met le .env dans wordpress
-# wp config create	--allow-root \
-#     --dbname=$MYSQL_DATABASE \
-#     --dbuser=$MYSQL_USER \
-#     --dbpass=$MYSQL_PASSWORD \
-#     --dbhost=mariadb:3306 --path='/var/www/wordpress'
+# on met le .env dans wordpress
+wp config create	--allow-root \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-# # auto rempli la page de perso
-# wp core install
+# auto rempli la page de perso
+wp core install
 
-# wp user create
+wp user create
 
 
 
@@ -42,47 +42,49 @@ sleep 10
 # mkdir -p /run/php/
 # php-fpm7.3 -F
 
+
+
 #!/bin/sh
 
 #check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
+# if [ -f ./wp-config.php ]
+# then
+# 	echo "wordpress already downloaded"
+# else
 
-####### MANDATORY PART ##########
+# ####### MANDATORY PART ##########
 
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+# 	#Download wordpress and all config file
+# 	wget http://wordpress.org/latest.tar.gz
+# 	tar xfz latest.tar.gz
+# 	mv wordpress/* .
+# 	rm -rf latest.tar.gz
+# 	rm -rf wordpress
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-###################################
+# 	#Inport env variables in the config file
+# 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+# 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+# 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+# 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+# 	cp wp-config-sample.php wp-config.php
+# ###################################
 
-####### BONUS PART ################
+# ####### BONUS PART ################
 
-## redis ##
+# ## redis ##
 
-	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
-  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
- 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-  	#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
- 	wp config set WP_REDIS_CLIENT phpredis --allow-root
-	wp plugin install redis-cache --activate --allow-root
-    wp plugin update --all --allow-root
-	wp redis enable --allow-root
+# 	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+#   	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+#  	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+#   	#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+#  	wp config set WP_REDIS_CLIENT phpredis --allow-root
+# 	wp plugin install redis-cache --activate --allow-root
+#     wp plugin update --all --allow-root
+# 	wp redis enable --allow-root
 
-###  end of redis part  ###
+# ###  end of redis part  ###
 
-###################################
-fi
+# ###################################
+# fi
 
-exec "$@"
+# exec "$@"
