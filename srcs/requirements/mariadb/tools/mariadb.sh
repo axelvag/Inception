95f9a6a002d7432bcffd4da3ec3c9c46d425a461
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-29 14:23:47 +0100

    add jlaisne work 2

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,6 +5,7 @@
 
 # service mysql start;
 mysql start;
+sleep 10;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
