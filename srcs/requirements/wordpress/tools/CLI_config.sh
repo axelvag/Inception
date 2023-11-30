9d676a601679e6a4635b2a49a0d33b45d9582569
Axel Vaganay <avaganay@z1r1p1.42lyon.fr>
2023-11-30 14:34:50 +0100

    reopload CLI without echo

--- srcs/requirements/wordpress/tools/CLI_config.sh
+++ srcs/requirements/wordpress/tools/CLI_config.sh
@@ -1,49 +1,46 @@
-# on attend que mariaDb soit up
-# sleep 10
+on attend que mariaDb soit up
+sleep 10
 
-# rm -rf /var/www/html/*
+rm -rf /var/www/html/*
 
-# # Download and install WordPress
+# Download and install WordPress
 # if [ -f /var/www/html/wp-config.php ]
 # then
 # 	echo "Wordpress already installer"
 # else
-# 	echo "Installing Wordpress"
+echo "Installing Wordpress"
 
-#     wp core download --allow-root --path=/var/www/html
+wp core download --allow-root --path=/var/www/html
 
-#     echo "1"
-#     # on met le .env dans wordpress
-#     wp config create --dbname=$MYSQL_DATABASE \
-#         --dbuser=$MYSQL_USER \
-#         --dbpass=$MYSQL_PASSWORD \
-#         --dbhost=$WORDPRESS_DB_HOST \
-#         --dbprefix=wp_ --allow-root
+# on met le .env dans wordpress
+wp config create --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=$WORDPRESS_DB_HOST \
+    --dbprefix=wp_ --allow-root
 
-#     cat /var/www/html/wp-config.php
+cat /var/www/html/wp-config.php
 
-#     echo "2"
-#     # auto rempli la page de perso
-#     wp core install --allow-root \
-#     --url="$DOMAIN_NAME" \
-#     --title="Inception" \
-#     --admin_user="$WORDPRESS_ADMIN_NAME" \
-#     --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-#     --admin_email="$WORDPRESS_ADMIN_EMAIL"
+# auto rempli la page de perso
+wp core install --allow-root \
+--url="$DOMAIN_NAME" \
+--title="Inception" \
+--admin_user="$WORDPRESS_ADMIN_NAME" \
+--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+--admin_email="$WORDPRESS_ADMIN_EMAIL"
 
-#     echo "3"
-#     wp user create --allow-root \
-#     "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-#     --user_pass="$WORDPRESS_USER_PASSWORD" \
-#     --role=subscriber
+wp user create --allow-root \
+"$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+--user_pass="$WORDPRESS_USER_PASSWORD" \
+--role=subscriber
 
 # fi
 
-# # Mets les droit sur le dossier d'installation de wp
-# chown -R www-data:www-data /var/www/html/
-# # Lance php-
-# echo 'Starting php-fpm7.3'
-# /usr/sbin/php-fpm7.3 -F
+# Mets les droit sur le dossier d'installation de wp
+chown -R www-data:www-data /var/www/html/
+# Lance php-
+echo 'Starting php-fpm7.3'
+/usr/sbin/php-fpm7.3 -F
 
 
 # echo "== Installing and setting up Wordpress =="
