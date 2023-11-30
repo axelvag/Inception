ed6fb8c415bad6ab223b16a53f9cce0a8f317efd
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-30 11:23:58 +0100

    change wp core download

--- srcs/requirements/wordpress/tools/CLI_config.sh
+++ srcs/requirements/wordpress/tools/CLI_config.sh
@@ -9,27 +9,28 @@ else
 	echo "Installing Wordpress"
 
     wp core download --allow-root --path=/var/www/html
+
+    # on met le .env dans wordpress
+    wp config create --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --dbprefix=wp_ --allow-root
+
+    # auto rempli la page de perso
+    wp core install --allow-root \
+    --url="$DOMAIN_NAME" \
+    --title="Inception" \
+    --admin_user="$WORDPRESS_ADMIN_NAME" \
+    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+    --admin_email="$WORDPRESS_ADMIN_EMAIL"
+
+    wp user create --allow-root \
+    "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+    --user_pass="$WORDPRESS_USER_PASSWORD" \
+    --role=subscriber
+
 fi
-
-# on met le .env dans wordpress
-wp config create --dbname=$MYSQL_DATABASE \
-    --dbuser=$MYSQL_USER \
-    --dbpass=$MYSQL_PASSWORD \
-    --dbhost=$WORDPRESS_DB_HOST \
-    --dbprefix=wp_ --allow-root
-
-# auto rempli la page de perso
-wp core install --allow-root \
-  --url="$DOMAIN_NAME" \
-  --title="Inception" \
-  --admin_user="$WORDPRESS_ADMIN_NAME" \
-  --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-  --admin_email="$WORDPRESS_ADMIN_EMAIL"
-
-wp user create --allow-root \
-  "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-  --user_pass="$WORDPRESS_USER_PASSWORD" \
-  --role=subscriber
 
 # Mets les droit sur le dossier d'installation de wp
 chown -R www-data:www-data /var/www/html/
