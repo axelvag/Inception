63838f18f8052e6630d4062b507c297a0bb77fd0
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-11-30 11:16:53 +0100

    change cli

--- srcs/requirements/wordpress/tools/CLI_config.sh
+++ srcs/requirements/wordpress/tools/CLI_config.sh
@@ -16,17 +16,17 @@ wp config create --dbname=$MYSQL_DATABASE \
     --dbprefix=wp_ --allow-root
 
 # auto rempli la page de perso
-wp core install --allow-root \
-  --url="$DOMAIN_NAME" \
-  --title="Inception" \
-  --admin_user="$WORDPRESS_ADMIN_NAME" \
-  --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-  --admin_email="$WORDPRESS_ADMIN_EMAIL"
+# wp core install #--allow-root \
+#   --url="$DOMAIN_NAME" \
+#   --title="Inception" \
+#   --admin_user="$WORDPRESS_ADMIN_NAME" \
+#   --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+#   --admin_email="$WORDPRESS_ADMIN_EMAIL"
 
-wp user create --allow-root \
-  "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-  --user_pass="$WORDPRESS_USER_PASSWORD" \
-  --role=subscriber
+# wp user create --allow-root \
+#   "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+#   --user_pass="$WORDPRESS_USER_PASSWORD" \
+#   --role=subscriber
 
 # Mets les droit sur le dossier d'installation de wp
 chown -R www-data:www-data /var/www/html/
