5fb514f9608a1496d93239342c96b575415c203a
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-12-01 11:34:17 +0100

    try revert

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,39 +1,38 @@
 
-if [ -f ./wp-config.php ]
-then
-	echo "Wordpress already installer"
-else
-	echo "Installing Wordpress"
+# if [ -f ./wp-config.php ]
+# then
+# 	echo "Wordpress already installer"
+# else
+# 	echo "Installing Wordpress"
 
-	wp core download --allow-root
+# 	wp core download --allow-root
 
-	until mysqladmin -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS ping && mariadb -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS -e "SHOW DATABASES;" | grep "$WORDPRESS_DB_NAME"; \
-		do
-		echo 'Waiting for mariadb'
-		sleep 10
-	done
+# 	until mysqladmin -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS ping && mariadb -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS -e "SHOW DATABASES;" | grep "$WORDPRESS_DB_NAME"; \
+# 		do
+# 		echo 'Waiting for mariadb'
+# 		sleep 10
+# 	done
 
-    echo "WP_SETUP.SH"
-    wp core config  --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_USER_PASS --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root
+#     echo "WP_SETUP.SH"
+#     wp core config  --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_USER_PASS --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root
     
-    wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+#     wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
     
-	wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --role=subscriber --user_pass=$WORDPRESS_USER_PASSWORD --display_name=$WORDPRESS_USER_DISPLAY --allow-root
-fi
+# 	wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --role=subscriber --user_pass=$WORDPRESS_USER_PASSWORD --display_name=$WORDPRESS_USER_DISPLAY --allow-root
+# fi
 
-# Mets les droit sur le dossier d'installation de wp
-chown -R www-data:www-data /var/www/html/
+# # Mets les droit sur le dossier d'installation de wp
+# chown -R www-data:www-data /var/www/html/
 
-# Lance php-
-echo 'Starting php-fpm7.3'
-/usr/sbin/php-fpm7.3 -F
+# # Lance php-
+# echo 'Starting php-fpm7.3'
+# /usr/sbin/php-fpm7.3 -F
 
 
 
 #!/bin/bash
 
 # Check if WordPress is already installed
-<<<<<<< HEAD
 # if [ -f /var/www/html/wp-config.php ]
 # then
 #     echo "Wordpress already installer"
@@ -76,9 +75,6 @@ echo 'Starting php-fpm7.3'
 
 
 
-
-
-=======
 if [ -f /var/www/html/wp-config.php ]
 then
     echo "Wordpress already installer"
@@ -109,7 +105,11 @@ fi
 
   # Set appropriate permissions
 chown -R www-data:www-data /var/www/html/
->>>>>>> parent of 006338d... evolve for avaganay
+
+# Start PHP-FPM
+/usr/sbin/php-fpm7.3 -F
+
+
 
 
 # # on attend que mariaDb soit up
