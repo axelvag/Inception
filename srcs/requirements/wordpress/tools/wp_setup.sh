39b24f0b8f8de427dff4464257b842603f27a766
Axel Vaganay <avaganay@z2r1p6.42lyon.fr>
2023-12-01 11:18:44 +0100

    clean .env

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,3 +1,34 @@
+
+if [ -f ./wp-config.php ]
+then
+	echo "Wordpress already installer"
+else
+	echo "Installing Wordpress"
+
+	wp core download --allow-root
+
+	until mysqladmin -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS ping && mariadb -hmariadb -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASS -e "SHOW DATABASES;" | grep "$WORDPRESS_DB_NAME"; \
+		do
+		echo 'Waiting for mariadb'
+		sleep 1
+	done
+
+    wp core config  --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_USER_PASS --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root
+    
+    wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+    
+	wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --role=subscriber --user_pass=$WORDPRESS_USER_PASSWORD --display_name=$WORDPRESS_USER_DISPLAY --allow-root
+fi
+
+# Mets les droit sur le dossier d'installation de wp
+chown -R www-data:www-data /var/www/html/
+
+# Lance php-
+echo 'Starting php-fpm7.3'
+/usr/sbin/php-fpm7.3 -F
+
+
+
 #!/bin/bash
 
 # Check if WordPress is already installed
@@ -5,41 +36,47 @@
 # then
 #     echo "Wordpress already installer"
 # else
-wp core download --allow-root
+# wp core download --allow-root
 
-sleep 10
+# sleep 10
 
-echo "1"
+# echo "1"
 
-wp config create --dbname=$MYSQL_DATABASE \
-                --dbuser=$MYSQL_USER \
-                --dbpass=$MYSQL_PASSWORD \
-                --dbhost=$WORDPRESS_DB_HOST \
-                --dbprefix=wp_ --allow-root
+# wp config create --dbname=$MYSQL_DATABASE \
+#                 --dbuser=$MYSQL_USER \
+#                 --dbpass=$MYSQL_PASSWORD \
+#                 --dbhost=$WORDPRESS_DB_HOST \
+#                 --dbprefix=wp_ --allow-root
 
-echo "2"
+# echo "2"
 
-# Install WordPress
-wp core install --allow-root \
---url="$DOMAIN_NAME" \
---title="Inception" \
---admin_user="$WORDPRESS_ADMIN_NAME" \
---admin_password="$WORDPRESS_ADMIN_PASSWORD" \
---admin_email="$WORDPRESS_ADMIN_EMAIL"
+# # Install WordPress
+# wp core install --allow-root \
+# --url="$DOMAIN_NAME" \
+# --title="Inception" \
+# --admin_user="$WORDPRESS_ADMIN_NAME" \
+# --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+# --admin_email="$WORDPRESS_ADMIN_EMAIL"
 
-echo "3"
+# echo "3"
 
-wp user create --allow-root \
-"$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
---user_pass="$WORDPRESS_USER_PASSWORD" \
---role=subscriber
-# fi
+# wp user create --allow-root \
+# "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+# --user_pass="$WORDPRESS_USER_PASSWORD" \
+# --role=subscriber
+# # fi
 
-  # Set appropriate permissions
-chown -R www-data:www-data /var/www/html/
+#   # Set appropriate permissions
+# chown -R www-data:www-data /var/www/html/
 
-# Start PHP-FPM
-/usr/sbin/php-fpm7.3 -F
+# # Start PHP-FPM
+# /usr/sbin/php-fpm7.3 -F
+
+
+
+
+
+
 
 # # on attend que mariaDb soit up
 # sleep 10
