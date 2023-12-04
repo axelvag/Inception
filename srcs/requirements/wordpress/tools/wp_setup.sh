1b1751ab519f66b02ff14ac6b9f3bacb7ca96446
Axel Vaganay <avaganay@z1r4p4.42lyon.fr>
2023-12-04 14:38:26 +0100

    change script CLI

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,32 +1,32 @@
 
 # Check if WordPress is already installed
-if [ -f /var/www/html/wp-config.php ]
-then
-    echo "Wordpress already installer"
-else
-  wp core download --allow-root
+# if [ -f /var/www/html/wp-config.php ]
+# then
+#     echo "Wordpress already installer"
+# else
+wp core download --allow-root
 
-  sleep 10
+sleep 10
 
-  wp config create --dbname=$MYSQL_DATABASE \
-                  --dbuser=$MYSQL_USER \
-                  --dbpass=$MYSQL_PASSWORD \
-                  --dbhost=$WORDPRESS_DB_HOST \
-                  --dbprefix=wp_ --allow-root
+wp config create --dbname=$MYSQL_DATABASE \
+                --dbuser=$MYSQL_USER \
+                --dbpass=$MYSQL_PASSWORD \
+                --dbhost=$WORDPRESS_DB_HOST \
+                --dbprefix=wp_ --allow-root
 
-  # Install WordPress
-  wp core install --allow-root \
-    --url="$DOMAIN_NAME" \
-    --title="Inception" \
-    --admin_user="$WORDPRESS_ADMIN_NAME" \
-    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-    --admin_email="$WORDPRESS_ADMIN_EMAIL"
+# Install WordPress
+wp core install --allow-root \
+  --url="$DOMAIN_NAME" \
+  --title="Inception" \
+  --admin_user="$WORDPRESS_ADMIN_NAME" \
+  --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+  --admin_email="$WORDPRESS_ADMIN_EMAIL"
 
-  wp user create --allow-root \
-    "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-    --user_pass="$WORDPRESS_USER_PASSWORD" \
-    --role=subscriber
-fi
+wp user create --allow-root \
+  "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+  --user_pass="$WORDPRESS_USER_PASSWORD" \
+  --role=subscriber
+# fi
 
 # Mets les droit sur le dossier d'installation de wp
 chown -R www-data:www-data /var/www/html/
