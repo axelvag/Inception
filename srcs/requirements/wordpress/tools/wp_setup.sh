c5e306a467aad8517cb8561a67af65d4b317ebb6
Axel Vaganay <avaganay@z1r4p4.42lyon.fr>
2023-12-04 15:19:50 +0100

    my wp_setup.sh

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -1,13 +1,16 @@
 
-#!/bin/bash
-
 # Check if WordPress is already installed
+if [ -f /var/www/html/wp-config.php ]
+then
+    echo "Wordpress already installer"
+else
 wp core download --allow-root
 
 sleep 10
 
-wp config create --dbname=$MYSQL_DATABASE \
-                --dbuser=$MYSQL_USER \
+# Remplissage du formulaire
+wp config create --dbname=$MYSQL_DATABASE \ # wordpress
+                --dbuser=$MYSQL_USER \ 
                 --dbpass=$MYSQL_PASSWORD \
                 --dbhost=$WORDPRESS_DB_HOST \
                 --dbprefix=wp_ --allow-root
@@ -24,50 +27,14 @@ wp user create --allow-root \
   "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
   --user_pass="$WORDPRESS_USER_PASSWORD" \
   --role=subscriber
+fi
 
-  # Set appropriate permissions
+# Mets les droit sur le dossier d'installation de wp
 chown -R www-data:www-data /var/www/html/
 
-# Start PHP-FPM
+# Lance php-
+echo 'Starting php-fpm7.3'
 /usr/sbin/php-fpm7.3 -F
-
-
-# # Check if WordPress is already installed
-# # if [ -f /var/www/html/wp-config.php ]
-# # then
-# #     echo "Wordpress already installer"
-# # else
-# wp core download --allow-root
-
-# sleep 10
-
-# # Remplissage du formulaire
-# wp config create --dbname=$MYSQL_DATABASE \ # wordpress
-#                 --dbuser=$MYSQL_USER \ 
-#                 --dbpass=$MYSQL_PASSWORD \
-#                 --dbhost=$WORDPRESS_DB_HOST \
-#                 --dbprefix=wp_ --allow-root
-
-# # Install WordPress
-# wp core install --allow-root \
-#   --url="$DOMAIN_NAME" \
-#   --title="Inception" \
-#   --admin_user="$WORDPRESS_ADMIN_NAME" \
-#   --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-#   --admin_email="$WORDPRESS_ADMIN_EMAIL"
-
-# wp user create --allow-root \
-#   "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-#   --user_pass="$WORDPRESS_USER_PASSWORD" \
-#   --role=subscriber
-# # fi
-
-# # Mets les droit sur le dossier d'installation de wp
-# chown -R www-data:www-data /var/www/html/
-
-# # Lance php-
-# echo 'Starting php-fpm7.3'
-# /usr/sbin/php-fpm7.3 -F
 
 
 
