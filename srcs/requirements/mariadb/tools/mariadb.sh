d35be6459ee2e8a2895c9b55de70d166271bd34c
Axel Vaganay <avaganay@z1r4p4.42lyon.fr>
2023-12-04 10:27:20 +0100

    restor me

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,40 +1,41 @@
-#!/bin/bash
+# Ce script créer avec MySQL le système de table
+# Lance mysql (start) puis applique mysql -u root(pour dire que je suis root) -e(pour specifier une cmd sql a exec) a chaque ligne du script
+# GRANT ALL PRIV pour donner tous les droits a l'utilisateur
+# FLUSH PRIVILEGES pour appliquer les changements
 
-# Start the MariaDB service inside the Docker container
-mysqld_safe &
-
-sleep 10
-
-# Create the WordPress database
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-
-# Create the MySQL user for WordPress
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# Grant privileges to the WordPress user on the WordPress database
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# Flush privilegess
+# service mysql start;
+mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-
-# Stop the MariaDB server gracefully
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
-# Start the MariaDB service inside the Docker container
 mysqld_safe
+# exec mysqld_safe
 
-# # Ce script créer avec MySQL le système de table
-# # Lance mysql (start) puis applique mysql -u root(pour dire que je suis root) -e(pour specifier une cmd sql a exec) a chaque ligne du script
-# # GRANT ALL PRIV pour donner tous les droits a l'utilisateur
-# # FLUSH PRIVILEGES pour appliquer les changements
+# #!/bin/bash
 
-# # service mysql start;
-# mysql start;
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# # Start the MariaDB service inside the Docker container
+# mysqld_safe &
+
+# sleep 10
+
+# # Create the WordPress database
+# mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+# # Create the MySQL user for WordPress
+# mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+# # Grant privileges to the WordPress user on the WordPress database
+# mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+# # Flush privilegess
 # mysql -e "FLUSH PRIVILEGES;"
+
+# # Stop the MariaDB server gracefully
 # mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+# # Start the MariaDB service inside the Docker container
 # mysqld_safe
-# # exec mysqld_safe
+
