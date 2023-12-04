2c14f906e3d56fc1fcd41c3c5d29066e82772ae2
Axel Vaganay <avaganay@z1r4p4.42lyon.fr>
2023-12-04 17:35:32 +0100

    try fix error atro

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -24,5 +24,6 @@ mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" # Create the MySQL use
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" # Grant privileges to the WordPress user on the WordPress database
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" # Flush privilegess
 mysql -e "FLUSH PRIVILEGES;" # Stop the MariaDB server gracefully
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown # Start the MariaDB service inside the Docker container
 mysqld_safe
