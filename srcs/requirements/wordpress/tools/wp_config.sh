dbe854de0def2fd35deebeca6f6a22e57096a3f8
Axel Vaganay <avaganay@z1r5p4.42lyon.fr>
2023-12-05 14:37:40 +0100

    axel change mariadb.cnf

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Check if WordPress is already installed
 wp core download --allow-root
 
 sleep 10
 
+# echo "MYSQL_DATABASE: $MYSQL_DATABASE"
+# echo "MYSQL_USER: $MYSQL_USER"
+# echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
+# echo "WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST"
+# echo "DOMAIN_NAME: $DOMAIN_NAME"
+# echo "WORDPRESS_ADMIN_NAME: $WORDPRESS_ADMIN_NAME"
+# echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD"
+# echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL"
+# echo "WORDPRESS_USER_NAME: $WORDPRESS_USER_NAME"
+# echo "WORDPRESS_USER_EMAIL: $WORDPRESS_USER_EMAIL"
+# echo "WORDPRESS_USER_PASSWORD: $WORDPRESS_USER_PASSWORD"
+
+# Remplissage du formulaire
 wp config create --dbname=$MYSQL_DATABASE \
                 --dbuser=$MYSQL_USER \
                 --dbpass=$MYSQL_PASSWORD \
                 --dbhost=$WORDPRESS_DB_HOST \
                 --dbprefix=wp_ --allow-root
 
-# Install WordPress
+# Installation de wordpress
 wp core install --allow-root \
   --url="$DOMAIN_NAME" \
   --title="Inception" \
@@ -19,13 +31,14 @@ wp core install --allow-root \
   --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
   --admin_email="$WORDPRESS_ADMIN_EMAIL"
 
+# Cree un utilisateur
 wp user create --allow-root \
   "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
   --user_pass="$WORDPRESS_USER_PASSWORD" \
   --role=subscriber
 
-# Set appropriate permissions
+# Mets les droit sur le dossier d'installation de wp
 chown -R www-data:www-data /var/www/html/
 
-# Start PHP-FPM
+# Lance php
 /usr/sbin/php-fpm7.3 -F
