d32d5129308e4a03142383a5845bca4c98c1d810
Axel Vaganay <avaganay@z1r5p4.42lyon.fr>
2023-12-05 13:53:54 +0100

    axel change mariadb.cnf

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+# Create the WordPress database
+# Ce script créer avec MySQL le système de table
+# Lance mysql (start) puis applique mysql -u root(pour dire que je suis root) -e(pour specifier une cmd sql a exec) a chaque ligne du script
+# GRANT ALL PRIV pour donner tous les droits a l'utilisateur
+# FLUSH PRIVILEGES pour appliquer les changements
+
 # Start the MariaDB service inside the Docker container
 mysqld_safe &
 
 sleep 10
 
-# Create the WordPress database
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-
-# Create the MySQL user for WordPress
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# Grant privileges to the WordPress user on the WordPress database
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# Flush privilegess
 mysql -e "FLUSH PRIVILEGES;"
 
 # Stop the MariaDB server gracefully
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-# Start the MariaDB service inside the Docker container
 mysqld_safe
